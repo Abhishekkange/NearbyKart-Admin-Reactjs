8684359d87ea82adfcb6a7e26369f910e9b240fc
Abhishek <Abhishekkange@gmail.com>
2023-11-27 19:07:28 +0530

    created user profile route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import Dashboard from './Components/Dashboard';
 import Sidebar from './Components/Sidebar';
 import AddBulkProduct from './Components/AddBulkProduct';
-
+import UserProfile from './Components/userProfileSection/UserProfile';
 function App() {
   return (
     <div className='flex w-full h-full flex-row'>
@@ -17,6 +17,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Dashboard />} />
             <Route path='/bulkProduct' element={<AddBulkProduct />} />
+            <Route path='/UserProfile' element={<UserProfile/>} />
           </Routes>
         </div>
       </Router>
