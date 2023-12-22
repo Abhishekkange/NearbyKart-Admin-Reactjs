612cf8d9e396df90e9475fad442476eb6f08070a
Abhishek <Abhishekkange@gmail.com>
2023-12-23 00:55:21 +0530

    color component

--- Frontend/src/Components/Bulk Add Section/CategoryAndSubcategory.jsx
+++ Frontend/src/Components/Bulk Add Section/CategoryAndSubcategory.jsx
@@ -1,124 +1,145 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import productContext from '../../Context/ProductContext';
+import axios from 'axios';
 
 const CategorySelector = () => {
+  const { productState, setProductState } = useContext(productContext);
 
+  const handleSaveAndContinue = () => {
+    const updatedProductState = {
+      category: selectedCategory,
+      subcategory: selectedSubcategory,
+      brand: selectedBrand,
+    };
 
-    
-    const {productState , setProductState} = useContext(productContext);
+    setProductState((prevState) => ({ ...prevState, ...updatedProductState }));
+  };
 
-    const handleSaveAndContinue = () => {
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedSubcategory, setSelectedSubcategory] = useState('');
+  const [selectedBrand, setSelectedBrand] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [subcategories, setSubcategories] = useState([]);
+  const [brands, setBrands] = useState([]);
 
-        const updatedProductState = {
-            
-            "category":selectedCategory,
-            "subcategory":selectedSubcategory,
-            "brand":selectedBrand
+  useEffect(() => {
+    fetchCategories();
+    fetchSubcategories();
+    fetchBrands();
+  }, []);
 
-        }
-
-        setProductState(prevState =>({...prevState,...updatedProductState}))
-
+  const fetchCategories = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/AllCategories');
+      setCategories(response.data);
+    } catch (error) {
+      console.error('Error fetching categories:', error);
     }
+  };
 
-    const [selectedCategory, setSelectedCategory] = useState('');
-    const [selectedSubcategory, setSelectedSubcategory] = useState('');
-    const [selectedBrand, setSelectedBrand] = useState('');
+  const fetchSubcategories = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/AllSubcategories');
+      setSubcategories(response.data);
+    } catch (error) {
+      console.error('Error fetching subcategories:', error);
+    }
+  };
 
-    // Sample data for categories, subcategories, and brands
-    const categories = ['Electronics', 'Clothing', 'Home & Garden'];
-    const subcategories = {
-        Electronics: ['Smartphones', 'Laptops', 'TVs'],
-        Clothing: ['T-shirts', 'Dresses', 'Jeans'],
-        'Home & Garden': ['Furniture', 'Kitchenware', 'Tools'],
-    };
-    const brands = ['Brand A', 'Brand B', 'Brand C'];
+  const fetchBrands = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/AllBrands');
+      setBrands(response.data);
+    } catch (error) {
+      console.error('Error fetching brands:', error);
+    }
+  };
 
-    const handleCategoryChange = (e) => {
-        const category = e.target.value;
-        setSelectedCategory(category);
-        setSelectedSubcategory('');
-    };
+  const handleCategoryChange = (e) => {
+    const category = e.target.value;
+    setSelectedCategory(category);
+    setSelectedSubcategory('');
+  };
 
-    const handleSubcategoryChange = (e) => {
-        const subcategory = e.target.value;
-        setSelectedSubcategory(subcategory);
-    };
+  const handleSubcategoryChange = (e) => {
+    const subcategory = e.target.value;
+    setSelectedSubcategory(subcategory);
+  };
 
-    const handleBrandChange = (e) => {
-        const brand = e.target.value;
-        setSelectedBrand(brand);
-    };
+  const handleBrandChange = (e) => {
+    const brand = e.target.value;
+    setSelectedBrand(brand);
+  };
 
-    return (
-        <div className="flex flex-col items-start">
-            <div className="mb-4">
-                <label style={{width:'200px'}} htmlFor="category" className="block mb-2 text-lg">
-                    Category:
-                </label>
-                <select
-                    style={{width:'450px'}}
-                    id="category"
-                    className="p-2 border rounded-md"
-                    value={selectedCategory}
-                    onChange={handleCategoryChange}
-                >
-                    <option value="">Select Category</option>
-                    {categories.map((category, index) => (
-                        <option key={index} value={category}>
-                            {category}
-                        </option>
-                    ))}
-                </select>
-            </div>
+  return (
+    <div className="flex flex-col items-start">
+      <div className="mb-4">
+        <label style={{ width: '200px' }} htmlFor="category" className="block mb-2 text-lg">
+          Category:
+        </label>
+        <select
+          style={{ width: '450px' }}
+          id="category"
+          className="p-2 border rounded-md"
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+        >
+          <option value="">Select Category</option>
+          {categories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
 
-            {selectedCategory && (
-                <div className="mb-4">
-                    <label htmlFor="subcategory" className="block mb-2 text-lg">
-                        Subcategory:
-                    </label>
-                    <select
-                        style={{width:'450px'}}
-                        id="subcategory"
-                        className="p-2 border rounded-md"
-                        value={selectedSubcategory}
-                        onChange={handleSubcategoryChange}
-                    >
-                        <option value="">Select Subcategory</option>
-                        {subcategories[selectedCategory].map((subcategory, index) => (
-                            <option key={index} value={subcategory}>
-                                {subcategory}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-            )}
+      <div className="mb-4">
+        <label htmlFor="subcategory" className="block mb-2 text-lg">
+          Subcategory:
+        </label>
+        <select
+          style={{ width: '450px' }}
+          id="subcategory"
+          className="p-2 border rounded-md"
+          value={selectedSubcategory}
+          onChange={handleSubcategoryChange}
+        >
+          <option value="">Select Subcategory</option>
+          {subcategories.map((subcategory, index) => (
+            <option key={index} value={subcategory}>
+              {subcategory}
+            </option>
+          ))}
+        </select>
+      </div>
 
-            <div>
-                <label htmlFor="brand" className="block mb-2 text-lg">
-                    Brand:
-                </label>
-                <select
-                    style={{width:'450px'}}
-                    id="brand"
-                    className="p-2 border rounded-md"
-                    value={selectedBrand}
-                    onChange={handleBrandChange}
-                >
-                    <option value="">Select Brand</option>
-                    {brands.map((brand, index) => (
-                        <option key={index} value={brand}>
-                            {brand}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <button onClick={handleSaveAndContinue} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded focus:outline-none focus:shadow-outline">
-                Save and Continue
-            </button>
-
-        </div>
-    );
+      <div>
+        <label htmlFor="brand" className="block mb-2 text-lg">
+          Brand:
+        </label>
+        <select
+          style={{ width: '450px' }}
+          id="brand"
+          className="p-2 border rounded-md"
+          value={selectedBrand}
+          onChange={handleBrandChange}
+        >
+          <option value="">Select Brand</option>
+          {brands.map((brand, index) => (
+            <option key={index} value={brand}>
+              {brand}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button
+        onClick={handleSaveAndContinue}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        Save and Continue
+      </button>
+    </div>
+  );
 };
 
 export default CategorySelector;
--- Frontend/src/Components/Master Section/EnterColor.jsx
+++ Frontend/src/Components/Master Section/EnterColor.jsx
@@ -1,11 +1,95 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react';
+import { TextField, List, ListItem, ListItemText, ListItemSecondaryAction, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { ChromePicker } from 'react-color';
+import axios, { Axios } from 'axios';
 
-const EnterColor = () => {
+const AddColorComponent = () => {
+  const [colorName, setColorName] = useState('');
+  const [selectedColor, setSelectedColor] = useState('#000000');
+  const [addedColors, setAddedColors] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
+    
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('http://localhost:3000/api/color');
+          setAddedColors(response.data);
+        } catch (error) {
+          console.error('Error fetching brands:', error);
+        }
+      };
+    
+
+  const handleColorChange = (color) => {
+    setSelectedColor(color.hex);
+  };
+
+  const handleAddColor = async() => {
+   
+    //API call here
+    const response = await axios.post('http://localhost:3000/api/color', { colorName,selectedColor }); 
+    console.log('color Added successfully');
+    setSelectedColor('#ffff')
+    setColorName('');
+    fetchData();
+
+
+  };
+  const handleDeleteColor = async (_id) => {
+
+    try {
+      const response = await axios.delete(`http://localhost:3000/api/color`,{_id});
+      fetchData(); // Fetch updated data after deletion
+    } catch (error) {
+      console.error('Error deleting color:', error);
+      // Handle error (e.g., show a message to the user, log, etc.)
+    }
+  };
   return (
-    <div>
-      This is colors section
-    </div>
-  )
-}
+    <div className="">
+      <div className="m-4  flex ">
+        <TextField
+          label="Color Name"
+          value={colorName}
+          onChange={(e) => setColorName(e.target.value)}
+          variant="outlined"
+        />
+        
+      <button
+        className="bg-blue-500 justify-center align-middle hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline m-4"
+        onClick={handleAddColor}
+      >
+        Add Color
+      </button>
+      </div>
 
-export default EnterColor
+      <div className="m-4">
+        <ChromePicker color={selectedColor} onChange={handleColorChange} />
+      </div>
+
+
+      <List style={{ width: '100%', maxWidth: 400 }}>
+        {addedColors.map((color) => (
+          <ListItem key={color._id}>
+            <div className="flex items-center">
+              <div className="w-10 h-10 rounded-full mr-4" style={{ backgroundColor: color.selectedColor }} />
+              <ListItemText primary={color.colorName} />
+              <ListItemText primary={color.selectedColor} />
+              <ListItemSecondaryAction>
+                <IconButton onClick={() => handleDeleteColor(color._id)}>
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </div>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export default AddColorComponent;
