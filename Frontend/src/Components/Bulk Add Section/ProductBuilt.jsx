f0a9f7282b2abe0d209231fc41ee8863ea12f29e
Abhishek <Abhishekkange@gmail.com>
2023-12-27 11:43:50 +0530

    some minor changes

--- Frontend/src/Components/Bulk Add Section/ProductBuilt.jsx
+++ Frontend/src/Components/Bulk Add Section/ProductBuilt.jsx
@@ -3,8 +3,7 @@
 import React from 'react';
 
 const ProductBuiltModal = ({ isOpen, onClose }) => {
-  retu
-  rn (
+  return (
     <div className={`fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center ${isOpen ? 'block' : 'hidden'}`}>
       <div className="bg-white p-6 rounded-lg shadow-md">
         <p className="text-lg font-semibold mb-4">Product has been built!</p>
