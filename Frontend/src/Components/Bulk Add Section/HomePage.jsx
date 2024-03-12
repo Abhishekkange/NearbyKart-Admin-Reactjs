0def3cc6f76c81c4ac4b7e5e7bdfed53e09bc50c
Abhishek <Abhishekkange@gmail.com>
2024-03-13 01:06:49 +0530

    login screen added inplace of home screen

--- Frontend/src/Components/Bulk Add Section/HomePage.jsx
+++ Frontend/src/Components/Bulk Add Section/HomePage.jsx
@@ -1,42 +1,64 @@
-import bg from '../../../src/assets/Icons/image.jpeg'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import Abhishek from '../../assets/Icons/mee.png'
+import './HomePage.css';
 
 function HomePage() {
-    return (
-      <div className="relative">
-        <img
-          style={{ width: '1500px', height: '100%', overflowY: 'hidden' }}
-          src={bg}
-          alt="i"
-        />
-        <Link to="/admin/bulkProduct">
-          <button
-            style={{
-              marginTop: '100px',
-              marginRight: '130px',
-              width: '150px',
-              height: '50px',
-            }}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute top-96 right-96"
-          >
-            Get Started
-          </button>
-          <Link to="/Register">
-          <button
-            style={{
-            
-              marginRight: '130px',
-              width: '150px',
-              height: '50px',
-            }}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute top-96 right-96"
-          >
-            Register
-          </button>
-        </Link>
-        </Link>
+
+  const[password,setPassword] =useState();
+
+
+
+
+  return (
+    <div className="flex h-screen background">
+      {/* Left Content */}
+      <div className="w-1/2 flex flex-col justify-center items-center">
+        
+        <div className="flex">
+        <h1 className="text-2xl nearbykart-txt text-blue-500">Nearby</h1>
+        <h1 className="text-2xl nearbykart-txt2 text-blue-500">Kart</h1>
+
+
+        </div>
+        {/* <h1 className="text-2xl nearbykart-txt text-blue-500">NearbyKart</h1> */}
+        <img src={Abhishek} alt="Your Image" className="myImage" />
       </div>
-    );
-  }
+
+      {/* Right Content */}
+      <div className="w-1/2 flex items-center justify-center">
+        <div className="max-w-md w-full bg-white shadow-lg rounded-lg p-8">
+          <h2 className="text-2xl font-bold mb-4">Login </h2>
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+             
+            />
+          </div>
+          <button className="bg-blue-500 login-btn hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            Login
+          </button>
+          <p className="text-sm mt-4">
+
+          <button className=" register-btn hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            Register my Store 
+          </button>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default HomePage;
