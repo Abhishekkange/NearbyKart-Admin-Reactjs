e6a3f3d869ba54ed041a8d5dd5c95f2fdcc25bbc
Abhishek Kange <84184526+Abhishekkange@users.noreply.github.com>
2024-09-10 00:17:41 +0530

    again changed

--- Frontend/src/Components/API/masterAPI.js
+++ Frontend/src/Components/API/masterAPI.js
@@ -1,7 +1,7 @@
 
 function API (){
 
-    const ip = 'https://nearby-kart-admin-bakend.vercel.app/';
+    const ip = 'https://nearby-kart-admin-bakend.vercel.app';
     const version = 'v1';
     const api = `${ip}/api/${version}/`
     return api;
